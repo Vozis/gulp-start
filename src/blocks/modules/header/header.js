34809998a856870c59721592f68f591554f5ddeb
ilyaSizov <sizov.ilya1996@gmail.com>
2022-05-04 12:34:21 +0300

    fix mistakes

--- src/blocks/modules/header/header.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.header__btn-menu').addEventListener('click', () => {
-  document.querySelectorAll('.menu').forEach((el) => {
-    el.classList.toggle('menu--open');
-  });
-});
--- src/js/import/components.js
+++ src/js/import/components.js
@@ -1,4 +1,1 @@
 import "../../../node_modules/jquery/dist/jquery.js";
-import "../../../node_modules/slick-carousel/slick/slick.js";
-import "../../../node_modules/mixitup/dist/mixitup.js";
-import "../../../node_modules/@fancyapps/ui/src/Fancybox/Fancybox.js";
